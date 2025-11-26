a58c2c9ab470ebe28cd97499931822af0edc12a7
sclash <andrea.sergi1@gmail.com>
2025-11-26 10:42:42 +0100

    starship

--- nixos/starship/starship.nix
+++ nixos/starship/starship.nix
@@ -1,10 +1,12 @@
-
 { config, lib, pkgs, unstable-pkgs, my-dotfiles, ... }:
 
 {
 
   programs.starship = {
     enable = true;
-    extraConfig = builtins.readFile "${my-dotfiles}/nixos/starship/starship.toml";
-	};
+    enableBashIntegrasion = true;
+    enableZshIntegrasion = true;
+    enableTranscience = true;
+    settings = builtins.readFile "${my-dotfiles}/nixos/starship/starship.toml";
+  };
 }
