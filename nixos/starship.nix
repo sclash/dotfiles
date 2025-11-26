29a2e30a1a22b267c77d6a20dc03e2b9c43b9de5
sclash <andrea.sergi1@gmail.com>
2025-11-26 11:58:33 +0100

    starship

--- nixos/starship.nix
+++ nixos/starship.nix
@@ -7,7 +7,10 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableTransience = true;
+    configPath = "${config.xdg.configHome}/starship/starship.toml";
+    # configPath = "$XDG_CONFIG_HOME/starship/starship.toml";
+
     # presets = builtins.readFile "${my-dotfiles}/starship/starship.toml";
-    presets =  [ "${my-dotfiles}/starship/starship.toml" ];
+    # presets =  [ "${my-dotfiles}/starship/starship.toml" ];
   };
 }
