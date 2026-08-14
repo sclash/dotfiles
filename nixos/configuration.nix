0b13244a38acd7bfd1a48f03ef9c090d2f2a496b
sclash <andrea.sergi1@gmail.com>
2026-08-14 18:33:20 +0200

    delete tmp files on reboot

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,6 +54,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmp.cleanOnBoot = true;
 
   programs.wshowkeys.enable = true;
 
