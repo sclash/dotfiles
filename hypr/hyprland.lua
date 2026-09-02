f8518930cf0ccdf9648dd18c03eb4fe96a0e3899
sclash <andrea.sergi1@gmail.com>
2026-09-02 21:56:23 +0200

    hyprpolkitagent enabled at startup

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -44,6 +44,7 @@ hl.on("hyprland.start", function ()
 		-- hl.exec_cmd("quickshell")
 		hl.exec_cmd("hyprpaper")
 		hl.exec_cmd("elephant")
+		hl.exec_cmd("hyprpolkitagent")
 		-- hl.exec_cmd("swaync")
 		-- hl.exec_cmd("swaync-client --daemon")
 		hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'") --# for GTK4 apps
