225798786ef65eeb196d463a8a1d853bbf0423f3
sclash <andrea.sergi1@gmail.com>
2026-09-04 20:27:37 +0200

    uninstalled rtk

--- nixos/home.nix
+++ nixos/home.nix
@@ -98,10 +98,10 @@
     ])
     ++ (with pkgs-unstable; [
       # markitdown-mcp
-      rtk
       # https://github.com/rtk-ai/rtk
       # rtk init -g
       # rtk init -g opencode
+      # rtk
       zennotes-desktop
     ]);
 
